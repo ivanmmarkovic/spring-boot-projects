5cfc0e619f20843d3d162ac4d5c8dadcd4f084d4
ivanmmarkovic <ivanmdmarkovic@gmail.com>
2020-05-01 00:29:51 +0200

    Add more custom queries

--- jpa-qustom-queries/jpacustomqueries/src/main/java/com/example/jpacustomqueries/repository/UserRepository.java
+++ jpa-qustom-queries/jpacustomqueries/src/main/java/com/example/jpacustomqueries/repository/UserRepository.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.example.jpacustomqueries.domain.User;
@@ -14,11 +15,15 @@ public interface UserRepository extends JpaRepository<User, Integer> {
 	/*
 	@Query("FROM User WHERE username = ?1")
 	public User findByUsername(String username);
-	*/
+	
 	@Query("SELECT o FROM User o WHERE o.username = ?1")
 	public User findByUsername(String username);
+	*/
+	@Query("SELECT o FROM User o WHERE o.username = :username")
+	public User findByUsername(@Param("username") String username);
 	
-	
+	@Query("FROM User ORDER BY username ASC")
+	public List<User> sortUsers();
 	
 	@Query("FROM User WHERE email LIKE %?#{[0]}%")
 	public List<User> getAllWithEmail(String email);
