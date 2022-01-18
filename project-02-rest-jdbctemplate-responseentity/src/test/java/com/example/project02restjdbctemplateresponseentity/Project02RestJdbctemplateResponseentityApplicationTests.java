5e3d19bda75d50d19ab19171ac45c66f47ad1db5
Ivan Markovic <ivanmdmarkovic@gmail.com>
2022-01-18 23:44:09 +0100

    Add project

---
+++ project-02-rest-jdbctemplate-responseentity/src/test/java/com/example/project02restjdbctemplateresponseentity/Project02RestJdbctemplateResponseentityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.project02restjdbctemplateresponseentity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project02RestJdbctemplateResponseentityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
