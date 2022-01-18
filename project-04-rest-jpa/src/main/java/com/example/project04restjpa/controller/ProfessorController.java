6198e907aac25b0ae5233aad6b41683fb6789c75
Ivan Markovic <ivanmdmarkovic@gmail.com>
2022-01-19 00:56:28 +0100

    ProfessorRepository, ProfessorController create, findAll

--- project-04-rest-jpa/src/main/java/com/example/project04restjpa/controller/ProfessorController.java
+++ project-04-rest-jpa/src/main/java/com/example/project04restjpa/controller/ProfessorController.java
@@ -1,5 +1,36 @@
 package com.example.project04restjpa.controller;
 
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.project04restjpa.model.Professor;
+import com.example.project04restjpa.repository.ProfessorRepository;
+
+@RestController
 public class ProfessorController {
 
+	private final ProfessorRepository professorRepository;
+	
+	public ProfessorController(ProfessorRepository professorRepository) {
+		this.professorRepository = professorRepository;
+	}
+	
+	@PostMapping("/professors")
+	public ResponseEntity<Professor> create(@RequestBody Professor professor){
+		Professor professorCreated = this.professorRepository.save(professor);
+		return new ResponseEntity<Professor>(professorCreated, HttpStatus.CREATED);
+	}
+	
+	@GetMapping("/professors")
+	public ResponseEntity<List<Professor>> findAll() {
+		List<Professor> professors = this.professorRepository.findAll();
+		return new ResponseEntity<>(professors, HttpStatus.OK);
+	}
+	
 }
--- project-04-rest-jpa/src/main/java/com/example/project04restjpa/repository/ProfessorRepository.java
+++ project-04-rest-jpa/src/main/java/com/example/project04restjpa/repository/ProfessorRepository.java
@@ -1,5 +1,11 @@
 package com.example.project04restjpa.repository;
 
-public interface ProfessorRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.project04restjpa.model.Professor;
+
+@Repository
+public interface ProfessorRepository extends JpaRepository<Professor, Integer> {
 
 }
