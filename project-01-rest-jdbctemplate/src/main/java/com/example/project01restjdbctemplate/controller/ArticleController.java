0b21d0bc3e01d8033c922236aef37a8f3befa2e6
Ivan Markovic <ivanmdmarkovic@gmail.com>
2022-01-19 00:14:30 +0100

    ArticleController @DeleteMapping('/articles/{id}')

--- project-01-rest-jdbctemplate/src/main/java/com/example/project01restjdbctemplate/controller/ArticleController.java
+++ project-01-rest-jdbctemplate/src/main/java/com/example/project01restjdbctemplate/controller/ArticleController.java
@@ -2,6 +2,7 @@ package com.example.project01restjdbctemplate.controller;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -41,6 +42,7 @@ public class ArticleController {
 		return this.articleRepository.update(id, article);
 	}
 	
+	@DeleteMapping("/articles/{id}")
 	public Article delete(@PathVariable int id) {
 		this.articleRepository.delete(id);
 		return null;
