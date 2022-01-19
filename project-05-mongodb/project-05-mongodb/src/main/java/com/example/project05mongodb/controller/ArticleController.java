4c132ece87157d2d0dc8bbdc012c56791c1d26fe
Ivan Markovic <ivanmdmarkovic@gmail.com>
2022-01-20 00:40:35 +0100

    Add Article repository

---
+++ project-05-mongodb/project-05-mongodb/src/main/java/com/example/project05mongodb/controller/ArticleController.java
@@ -0,0 +1,57 @@
+package com.example.project05mongodb.controller;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.project05mongodb.model.Article;
+import com.example.project05mongodb.repository.ArticleRepository;
+
+@RestController
+public class ArticleController {
+	
+	private ArticleRepository articleRepository;
+	public ArticleController(ArticleRepository articleRepository) {
+		this.articleRepository = articleRepository;
+	}
+	
+	@PostMapping("/articles")
+	public ResponseEntity<Article> create(@RequestBody Article article) {
+		return new ResponseEntity<Article>(
+			this.articleRepository.save(article),
+			HttpStatus.CREATED
+		);
+	}
+	
+	@GetMapping("/articles")
+	public ResponseEntity<List<Article>> findAll() {
+		return new ResponseEntity<List<Article>>(
+			this.articleRepository.findAll(),
+			HttpStatus.OK
+		);
+	}
+	
+	@GetMapping("/articles/{id}")
+	public ResponseEntity<Article> findbyId(@PathVariable String id){
+		return new ResponseEntity<Article>(
+			this.articleRepository.findById(id).orElse(null),
+			HttpStatus.OK
+		);
+	}
+	
+	@DeleteMapping()
+	ResponseEntity<Article> deleteById(@PathVariable String id){
+		this.articleRepository.deleteById(id);
+		return new ResponseEntity<>(
+			null,
+			HttpStatus.NO_CONTENT
+		);
+	}
+}
