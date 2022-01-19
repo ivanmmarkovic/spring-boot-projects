eafc544caf575d31d49afcf97f56f5d46f2eeae8
Ivan Markovic <ivanmdmarkovic@gmail.com>
2022-01-20 00:27:28 +0100

    mongodb project

---
+++ project-05-mongodb/project-05-mongodb/src/main/java/com/example/project05mongodb/Project05MongodbApplication.java
@@ -0,0 +1,13 @@
+package com.example.project05mongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project05MongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project05MongodbApplication.class, args);
+	}
+
+}
---
+++ project-05-mongodb/project-05-mongodb/src/test/java/com/example/project05mongodb/Project05MongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.project05mongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project05MongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
