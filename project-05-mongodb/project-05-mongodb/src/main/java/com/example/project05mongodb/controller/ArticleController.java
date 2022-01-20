fbf291845744e51d1949bc7ad82b22c8ff899679
Ivan Markovic <ivanmdmarkovic@gmail.com>
2022-01-20 01:08:38 +0100

    ArticlController finished

--- project-05-mongodb/project-05-mongodb/src/main/java/com/example/project05mongodb/controller/ArticleController.java
+++ project-05-mongodb/project-05-mongodb/src/main/java/com/example/project05mongodb/controller/ArticleController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -42,6 +43,16 @@ public class ArticleController {
 	public ResponseEntity<Article> findbyId(@PathVariable String id){
 		return new ResponseEntity<Article>(
 			this.articleRepository.findById(id).orElse(null),
+			HttpStatus.OK
+		);
+	}
+	
+	@PutMapping("/articles/{id}")
+	public ResponseEntity<Article> update(@PathVariable String id, @RequestBody Article article){
+		article.setId(id);
+		Article articleUpdated = this.articleRepository.save(article);
+		return new ResponseEntity<Article>(
+			articleUpdated,
 			HttpStatus.OK
 		);
 	}
